85b45475231fcee32441ae09a0786ccd7585ac9d
AdrianFoxy <drugakadrian@gmail.com>
2025-04-13 17:39:07 +0300

    DI for right clean arch

--- API/Program.cs
+++ API/Program.cs
@@ -1,10 +1,8 @@
 using API.Middleware;
 using Core.Entities;
-using Core.Interfaces;
+using Infrastructure;
 using Infrastructure.Data;
-using Infrastructure.Services;
 using Microsoft.EntityFrameworkCore;
-using StackExchange.Redis;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,34 +10,13 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddControllers();
-builder.Services.AddDbContext<StoreContext>(opt =>
-{
-    opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
 
-builder.Services.AddScoped<IBookRepository, BookRepository>();
-builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+builder.Services.AddInfrastructure(builder.Configuration);
 
 builder.Services.AddAuthorization();
 
-builder.Services.AddIdentityApiEndpoints<AppUser>(options =>
-{
-    options.Password.RequireLowercase = false;
-    options.Password.RequireUppercase = false;
-    options.Password.RequireNonAlphanumeric = false;
-    options.Password.RequiredLength = 6;
-})
-.AddEntityFrameworkStores<StoreContext>();
-
+builder.Services.AddIdentityApiEndpoints<AppUser>();
 builder.Services.AddCors();
-builder.Services.AddSingleton<IConnectionMultiplexer>(config =>
-{
-    var connectionString = builder.Configuration.GetConnectionString("Redis")
-        ?? throw new Exception("Cannot get redis connection string");
-    var configuration = ConfigurationOptions.Parse(connectionString, true);
-    return ConnectionMultiplexer.Connect(configuration);
-});
-builder.Services.AddSingleton<ICartService, CartService>();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
---
+++ Infrastructure/DependencyInjection.cs
@@ -0,0 +1,47 @@
+﻿using Core.Entities;
+using Core.Interfaces;
+using Infrastructure.Data;
+using Infrastructure.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using StackExchange.Redis;
+
+namespace Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(
+            this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.AddDbContext<StoreContext>(opt =>
+            {
+                opt.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+            });
+
+            services.AddScoped<IBookRepository, BookRepository>();
+            services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+
+            services.AddSingleton<IConnectionMultiplexer>(config =>
+            {
+                var redisConnectionString = configuration.GetConnectionString("Redis")
+                    ?? throw new Exception("Cannot get redis connection string");
+                var redisConfiguration = ConfigurationOptions.Parse(redisConnectionString, true);
+                return ConnectionMultiplexer.Connect(redisConfiguration);
+            });
+            services.AddSingleton<ICartService, CartService>();
+
+            services.AddIdentityCore<AppUser>(options =>
+            {
+                options.Password.RequireLowercase = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequiredLength = 6;
+            })
+            .AddEntityFrameworkStores<StoreContext>();
+
+            return services;
+        }
+    }
+}
